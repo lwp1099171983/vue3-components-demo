d65e7d67cb0b1c3de8ab48cafe1724a0d5f2b849
linweipeng1 <linweipeng1@bosssoft.com.cn>
2024-02-20 11:14:39 +0800

    update

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,27 +1,15 @@
+/* eslint-env node */
+require('@rushstack/eslint-patch/modern-module-resolution')
+
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-    node: true,
-  },
-  extends: ["plugin:vue/vue3-essential"],
-  overrides: [
-    {
-      env: {
-        node: true,
-      },
-      files: [".eslintrc.{js,cjs}"],
-      parserOptions: {
-        sourceType: "script",
-      },
-    },
+  root: true,
+  'extends': [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/eslint-config-typescript',
+    '@vue/eslint-config-prettier/skip-formatting'
   ],
   parserOptions: {
-    ecmaVersion: "latest",
-    parser: "@typescript-eslint/parser",
-    sourceType: "module",
-  },
-  rules: {
-    semi: 2,
-  },
-};
+    ecmaVersion: 'latest'
+  }
+}
